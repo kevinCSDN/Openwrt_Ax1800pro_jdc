b6fffe563439edede2e040f2dc52516fe39a4d09
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2025-01-04 08:41:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,7 +22,15 @@
 #cp -r openwrt-packages/luci-app-netdata package/
 #rm -rf openwrt-packages
 
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+./scripts/feeds install -a 
 
 
 git clone https://github.com/f8q8/luci-app-autoreboot package/luci-app-autoreboot
